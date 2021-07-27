0d1e5974585998986e5351271ffb35119e616c29
Aleksandr <mahnev.web@gmail.com>
2021-07-27 19:53:22 +0700

    initial

--- App.js
+++ App.js
@@ -5,7 +5,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.text}>Hello!</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +14,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'black',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  text:{
+    fontSize: 26, 
+    color: '#fff',
+    borderStyle: 'dotted',
+    borderWidth: 1,
+    padding: 5,
+    borderRadius: 10,
+    borderColor: 'pink',
   },
 });
